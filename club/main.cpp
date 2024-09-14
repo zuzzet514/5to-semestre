acb589c8c0898fe48ce0ea13ee6902ba5ef8a00a
Zuzzet Suarez <zuzzet.hs14@gmail.com>
2024-09-14 11:03:42 -0600

    IO en c++

---
+++ club/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+int k = 0;
+bool x = false;
+std::string h = "";
+
+int main()
+{
+    std::cout << "Ingresa el valor de k: ";
+    std::cin >> k;
+    std::cout << "el valor de k es: " << k;
+}
