ab726093d7397de55f3b4d4506c6527e8dc7e1ea
Hernandez Suarez Zuzzet <zS22017025@estudiantes.uv.mx>
2024-10-24 16:10:13 -0600

    selenium

---
+++ pruebas/Selenium/src/main/java/Login.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        /* //google
+
+
+        driver.get("https://www.google.com/");
+
+        System.out.println("Título de la página: " + driver.getTitle());
+
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("selenium");
+
+        element.submit();
+
+        //driver.quit();
+        */
+
+        driver.manage().window().maximize();
+        // duckduckgo
+
+        driver.get("https://duckduckgo.com/");
+
+        System.out.println("Título de la página: " + driver.getTitle());
+
+        WebElement element = driver.findElement(By.id("searchbox_input"));
+        element.sendKeys("monsta x");
+
+        element.submit();
+
+    }
+}
