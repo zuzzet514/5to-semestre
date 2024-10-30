d9fea54449d277e5832efc916c599d32203cf5c2
Hernandez Suarez Zuzzet <zS22017025@estudiantes.uv.mx>
2024-10-30 17:42:59 -0600

    seleniummm

---
+++ pruebas/Selenium/src/main/java/Texto.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Texto {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://diagramasuml.com/paquetes/#google_vignette");
+
+        String xpath = "//p[not(@id) and not(@class) and not(@name)]";
+
+        List<WebElement> paragraphs = driver.findElements(By.xpath(xpath));
+
+        for (WebElement paragraph : paragraphs) {
+            System.out.println(paragraph.getText());
+        }
+    }
+}
