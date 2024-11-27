0926e4a93345a9bf608c4d74add6f97d333d5724
Zuzzet Suarez <zuzzet.hs14@gmail.com>
2024-11-27 14:28:27 -0600

    YT2MP3 CoR first try

---
+++ tecnologias/bloque4/YT2MP3/ParseYoutubeLinkHandler.js
@@ -0,0 +1,18 @@
+class ParserYoutubeLinkHandler extends Handler{
+    async handle(input) {
+
+        const videoIdMatch = input.match(/(?:\?v=|&v=|youtu\.be\/|youtube\.com\/embed\/)([^&\s]+)/);
+        const videoId = videoIdMatch ? videoIdMatch[1] : input.trim();
+
+
+        if (!videoId || videoId === "") {
+            console.error("Error: Invalid input. Please enter a valid YT link or video ID")
+            return;
+        }
+
+        console.log(`Extracted video ID ${videoId}`);
+        return super.handle(videoId);
+    }
+
+
+}
